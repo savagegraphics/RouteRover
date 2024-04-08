6bd0566ea9c14a6c0b78fdd8f2aee9fbc3cda70d
olawaleraji <savagedotjsx@gmail.com>
2024-04-08 08:38:21 -0700

    “This”

---
+++ app/Home/RentBike.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const rentbike = (props: Props) => {
+  return <div>rentbike</div>
+}
+
+export default rentbike
---
+++ app/Home/RentCar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const rentcar = (props: Props) => {
+  return <div>rentcar</div>
+}
+
+export default rentcar
