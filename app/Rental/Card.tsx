22667e93289ab32c92e221b23ac678539d60b122
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-04-12 17:13:00 +0100

    before break

--- app/Rental/Card.tsx
+++ app/Rental/Card.tsx
@@ -225,16 +225,6 @@ const Card = () => {
 
   return (
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-      <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-          The Blog
-        </h2>
-        <p className="mt-1 text-gray-600 dark:text-gray-400">
-          See how game-changing companies are making the most of every
-          engagement with Preline.
-        </p>
-      </div>
-
       <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-6">
         {cardDetails.map((card, index) => (
           <a
