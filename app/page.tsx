1f80c1e9ddeb2dea32b26309402c09f6ecfbaa2e
olawaleraji <savagedotjsx@gmail.com>
2024-04-16 04:14:06 -0700

    Tuesday morning first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 import React from 'react'
-import Navbar from './Nav'
-import NavbarOne from './Navbar'
+// import Navbar from './Nav'
+import Navbar from './Navbar'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div>
-      <NavbarOne />
+      <Navbar />
       {/* <Navbar /> */}
     </div>
   )
