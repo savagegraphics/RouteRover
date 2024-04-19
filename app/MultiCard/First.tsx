c6876d4014b300d0a7451d80d6f0618d457dcb57
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-04-19 16:43:54 +0100

    datepicker

--- app/MultiCard/First.tsx
+++ app/MultiCard/First.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, ButtonGroup } from "@nextui-org/react";
+import Link from "next/link";
 
 type Props = {};
 
@@ -7,69 +8,73 @@ const First = (props: Props) => {
   return (
     <section className="p-4 lg:p-8 dark:bg-gray-100 dark:text-gray-800">
       <div className="container mx-auto space-y-12">
-        <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-          <img
-            src="https://images.unsplash.com/photo-1617788138017-80ad40651399?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRlc2xhfGVufDB8fDB8fHww"
-            alt=""
-            className="h-80 dark:bg-gray-500 aspect-video rounded-xl"
-          />
-          <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-            <span className="text-xs uppercase dark:text-gray-600">
-              Join, it&apos;s free
-            </span>
-            <h3 className="text-3xl font-bold">Rent A Car</h3>
-            <div className="flex gap-4 items-center mt-4">
-              <Button size="sm">COMFORT</Button>
-              <Button size="sm">LUXURY</Button>
-              <Button size="sm">PRESTIGE</Button>
+        <Link href="/rent-car">
+          <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
+            <img
+              src="https://images.unsplash.com/photo-1617788138017-80ad40651399?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRlc2xhfGVufDB8fDB8fHww"
+              alt=""
+              className="h-80 dark:bg-gray-500 aspect-video rounded-xl"
+            />
+            <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
+              <span className="text-xs uppercase dark:text-gray-600">
+                Join, it&apos;s free
+              </span>
+              <h3 className="text-3xl font-bold">Rent A Car</h3>
+              <div className="flex gap-4 items-center mt-4">
+                <Button size="sm">COMFORT</Button>
+                <Button size="sm">LUXURY</Button>
+                <Button size="sm">PRESTIGE</Button>
+              </div>
+              <p className="my-6 dark:text-gray-600">
+                Unlock your journey&apos;s potential with RouteRover&apos;s
+                Rent-A-Car feature. Select from a range of reliable vehicles
+                tailored to your needs and preferences. Enjoy seamless booking
+                and transparent pricing, backed by our dedicated support team.
+                Your adventure begins here.
+              </p>
+              <button
+                type="button"
+                className="self-start text-white bg-blue-500 px-6 py-2 rounded-lg"
+              >
+                Rent Car
+              </button>
             </div>
-            <p className="my-6 dark:text-gray-600">
-              Unlock your journey&apos;s potential with RouteRover&apos;s
-              Rent-A-Car feature. Select from a range of reliable vehicles
-              tailored to your needs and preferences. Enjoy seamless booking and
-              transparent pricing, backed by our dedicated support team. Your
-              adventure begins here.
-            </p>
-            <button
-              type="button"
-              className="self-start text-white bg-blue-500 px-6 py-2 rounded-lg"
-            >
-              Rent Car
-            </button>
           </div>
-        </div>
-        <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-          <img
-            src="https://images.unsplash.com/photo-1616889154907-0c5f6721dcba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW90b2N5Y2xlfGVufDB8fDB8fHww"
-            alt=""
-            className="h-80 dark:bg-gray-500 aspect-video rounded-xl"
-          />
-          <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
-            <span className="text-xs uppercase dark:text-gray-600">
-              Join, it&apos;s free
-            </span>
-            <h3 className="text-3xl font-bold">Rent A Bike </h3>
-            <div className="flex gap-4 items-center mt-4">
-              <Button size="sm">COMFORT</Button>
-              <Button size="sm">LUXURY</Button>
-              <Button size="sm">PRESTIGE</Button>
+        </Link>
+        <Link href="/rent-bike">
+          <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
+            <img
+              src="https://images.unsplash.com/photo-1616889154907-0c5f6721dcba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW90b2N5Y2xlfGVufDB8fDB8fHww"
+              alt=""
+              className="h-80 dark:bg-gray-500 aspect-video rounded-xl"
+            />
+            <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-50">
+              <span className="text-xs uppercase dark:text-gray-600">
+                Join, it&apos;s free
+              </span>
+              <h3 className="text-3xl font-bold">Rent A Bike </h3>
+              <div className="flex gap-4 items-center mt-4">
+                <Button size="sm">COMFORT</Button>
+                <Button size="sm">LUXURY</Button>
+                <Button size="sm">PRESTIGE</Button>
+              </div>
+              <p className="my-6 dark:text-gray-600">
+                Discover the freedom of the open road with RouteRover&apos;s
+                Rent-A-Bike feature. Choose from a variety of high-quality bikes
+                to suit your adventure, whether it&apos;s exploring the city
+                streets or tackling rugged trails. With easy booking and
+                transparent pricing, renting a bike with RouteRover is a breeze.
+                Start your journey today
+              </p>
+              <button
+                type="button"
+                className="self-start text-white bg-blue-500 px-6 py-2 rounded-lg"
+              >
+                Rent Bike
+              </button>
             </div>
-            <p className="my-6 dark:text-gray-600">
-              Discover the freedom of the open road with RouteRover&apos;s
-              Rent-A-Bike feature. Choose from a variety of high-quality bikes
-              to suit your adventure, whether it&apos;s exploring the city
-              streets or tackling rugged trails. With easy booking and
-              transparent pricing, renting a bike with RouteRover is a breeze.
-              Start your journey today
-            </p>
-            <button
-              type="button"
-              className="self-start text-white bg-blue-500 px-6 py-2 rounded-lg"
-            >
-              Rent Car
-            </button>
           </div>
-        </div>
+        </Link>
         <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
           <img
             src="https://images.unsplash.com/photo-1558981403-c5f9899a28bc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vdG9yY3ljbGUlMjBhbmQlMjAlMjBleG90aWNjYXJ8ZW58MHx8MHx8fDA%3D"
--- app/rent-bike/page.tsx
+++ app/rent-bike/page.tsx
@@ -1,61 +1,61 @@
-'use client'
-import React, { useState } from 'react'
-import Home from '../Home'
-import Bikes from '../Bike'
-import Contact from '../Contact'
-import Share from '../Sh'
-import Link from 'next/link'
-import Cars from '../Cars'
+"use client";
+import React, { useState } from "react";
+import Home from "../Rental/CarHome";
+import Bikes from "../Bike";
+import Contact from "../Contact";
+import Share from "../Sh";
+import Link from "next/link";
+import Cars from "../Cars";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [currentPage, setCurrentPage] = useState('Home')
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState("Home");
 
   const navigateTo = (page: string) => {
-    setCurrentPage(page)
+    setCurrentPage(page);
     // Close the menu when a page is selected
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
   return (
     <div>
-      <nav className='relative bg-white dark:bg-gray-900'>
-        <div className='container px-6 py-4 mx-auto md:flex md:justify-between md:items-center'>
-          <div className='flex items-center justify-between'>
-            <Link href='/'>
-              <h1 className='flex-none italic text-xl font-semibold'>
+      <nav className="relative bg-white dark:bg-gray-900">
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <Link href="/">
+              <h1 className="flex-none italic text-xl font-semibold">
                 RouteRover
               </h1>
             </Link>
-            <div className='flex md:hidden'>
+            <div className="flex md:hidden">
               <button
                 onClick={() => setIsOpen(!isOpen)}
-                type='button'
-                className='text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400'
-                aria-label='toggle menu'
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
               >
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='w-6 h-6'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
                   {!isOpen ? (
                     <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M4 6h16M4 12h16m-7 6h7'
+                      d="M4 6h16M4 12h16m-7 6h7"
                     />
                   ) : (
                     <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       strokeWidth={2}
-                      d='M6 18L18 6M6 6l12 12'
+                      d="M6 18L18 6M6 6l12 12"
                     />
                   )}
                 </svg>
@@ -64,76 +64,76 @@ const Navbar = (props: Props) => {
           </div>
           <div
             className={`${
-              isOpen ? 'block' : 'hidden'
+              isOpen ? "block" : "hidden"
             } md:flex md:items-center md:justify-between`}
           >
-            <div className='flex flex-col md:flex-row md:mx-6'>
+            <div className="flex flex-col md:flex-row md:mx-6">
               <a
                 className={`my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 ${
-                  currentPage === 'Home'
-                    ? 'hover:text-blue-500 dark:hover:text-blue-400'
-                    : 'hover:text-gray-600 dark:hover:text-gray-300'
+                  currentPage === "Home"
+                    ? "hover:text-blue-500 dark:hover:text-blue-400"
+                    : "hover:text-gray-600 dark:hover:text-gray-300"
                 } md:mx-4 md:my-0`}
-                href='#'
-                onClick={() => navigateTo('Home')}
+                href="#"
+                onClick={() => navigateTo("Home")}
               >
                 Home
               </a>
               <a
                 className={`my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 ${
-                  currentPage === 'Cars'
-                    ? 'hover:text-blue-500 dark:hover:text-blue-400'
-                    : 'hover:text-gray-600 dark:hover:text-gray-300'
+                  currentPage === "Cars"
+                    ? "hover:text-blue-500 dark:hover:text-blue-400"
+                    : "hover:text-gray-600 dark:hover:text-gray-300"
                 } md:mx-4 md:my-0`}
-                href='#'
-                onClick={() => navigateTo('Cars')}
+                href="#"
+                onClick={() => navigateTo("Cars")}
               >
                 Cars
               </a>
               <a
                 className={`my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 ${
-                  currentPage === 'Bikes'
-                    ? 'hover:text-blue-500 dark:hover:text-blue-400'
-                    : 'hover:text-gray-600 dark:hover:text-gray-300'
+                  currentPage === "Bikes"
+                    ? "hover:text-blue-500 dark:hover:text-blue-400"
+                    : "hover:text-gray-600 dark:hover:text-gray-300"
                 } md:mx-4 md:my-0`}
-                href='#'
-                onClick={() => navigateTo('Bikes')}
+                href="#"
+                onClick={() => navigateTo("Bikes")}
               >
                 Bikes
               </a>
               <a
                 className={`my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 ${
-                  currentPage === 'Share'
-                    ? 'hover:text-blue-500 dark:hover:text-blue-400'
-                    : 'hover:text-gray-600 dark:hover:text-gray-300'
+                  currentPage === "Share"
+                    ? "hover:text-blue-500 dark:hover:text-blue-400"
+                    : "hover:text-gray-600 dark:hover:text-gray-300"
                 } md:mx-4 md:my-0`}
-                href='#'
-                onClick={() => navigateTo('Share')}
+                href="#"
+                onClick={() => navigateTo("Share")}
               >
                 Share
               </a>
               <a
                 className={`my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 ${
-                  currentPage === 'Contact'
-                    ? 'hover:text-blue-500 dark:hover:text-blue-400'
-                    : 'hover:text-gray-600 dark:hover:text-gray-300'
+                  currentPage === "Contact"
+                    ? "hover:text-blue-500 dark:hover:text-blue-400"
+                    : "hover:text-gray-600 dark:hover:text-gray-300"
                 } md:mx-4 md:my-0`}
-                href='#'
-                onClick={() => navigateTo('Contact')}
+                href="#"
+                onClick={() => navigateTo("Contact")}
               >
                 Contact
               </a>
             </div>
-            <div className='flex flex-col mt-6 lg:mt-2 space-y-3 lg:space-y-0 lg:flex-row'>
+            <div className="flex flex-col mt-6 lg:mt-2 space-y-3 lg:space-y-0 lg:flex-row">
               <a
-                href='#'
-                className='block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700'
+                href="#"
+                className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700"
               >
                 Sign In
               </a>
               <a
-                href='#'
-                className='block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300'
+                href="#"
+                className="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300"
               >
                 Sign Up
               </a>
@@ -142,13 +142,13 @@ const Navbar = (props: Props) => {
         </div>
       </nav>
       {/* Render content based on currentPage state */}
-      {currentPage === 'Home' && <Home />}
-      {currentPage === 'Cars' && <Cars />}
-      {currentPage === 'Bikes' && <Bikes />}
-      {currentPage === 'Contact' && <Contact />}
-      {currentPage === 'Share' && <Share />}
+      {currentPage === "Home" && <Home />}
+      {currentPage === "Cars" && <Cars />}
+      {currentPage === "Bikes" && <Bikes />}
+      {currentPage === "Contact" && <Contact />}
+      {currentPage === "Share" && <Share />}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
