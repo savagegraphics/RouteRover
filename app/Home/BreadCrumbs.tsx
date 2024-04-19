f7b9ec56d7448d5ad5bea2ddffb6fbbd81ca2e5c
Project SURVIVOR <161699187+olawaleraji@users.noreply.github.com>
2024-04-19 12:30:04 +0100

    datepicker :=

--- app/Home/BreadCrumbs.tsx
+++ app/Home/BreadCrumbs.tsx
@@ -9,21 +9,24 @@ const BreadCrumbs = (props: Props) => {
     <div className="mb-16">
       <div className="relative px-4 sm:px-0">
         <div className="absolute inset-0 bg-gray-900 h-1/2" />
-        <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded-xl shadow sm:divide-y-0 sm:max-w-screen-sm md:grid-cols-3 lg:grid-cols-3 lg:max-w-screen-md">
+        <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded-xl shadow sm:divide-y-0 sm:max-w-screen-sm md:grid-cols-3 lg:grid-cols-3 lg:max-w-screen-lg">
           <div className="inline-block p-8 text-center items-center">
-            <p className="font-bold tracking-wide text-gray-800">
+            <div className="font-bold tracking-wide text-gray-800">
+              <div className="font-bold text-xl pb-2">Location</div>
               <Selectone />
-            </p>
+            </div>
           </div>
           <div className="inline-block p-8 text-center items-center">
-            <p className="font-bold tracking-wide text-gray-800">
+            <div className="font-bold tracking-wide text-gray-800">
+              <div className="font-bold text-xl pb-2">Location</div>
               <Select />
-            </p>
+            </div>
           </div>
           <div className="inline-block p-8 text-center items-center">
-            <p className="font-bold tracking-wide text-gray-800">
+            <div className="font-bold tracking-wide text-gray-800">
+              <div className="font-bold text-xl pb-2">Pick Up</div>
               <Select />
-            </p>
+            </div>
           </div>
         </div>
       </div>
